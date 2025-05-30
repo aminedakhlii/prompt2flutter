2812f7fa405e55b368e80f8d306ba21b495464d0
Amine Dakhli <amine.dakhli@medtech.tn>
2025-05-30 18:44:25 +0100

    more flutter UI samples for RAG

--- backend/lib/providers/settings_provider.dart
+++
@@ -1,8 +0,0 @@
-
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../core/network/api_network.dart';
-
-final apiServicesProvider = Provider<NetworkApiService>((ref) {
-  return NetworkApiService(); // Instance of API service
-});
-
